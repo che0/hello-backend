47f18003063af57460a1454b0f948a86af0f8c7b
Petr Novak 2 <petr.novak2@firma.seznam.cz>
2019-03-27 19:57:27 +0100

    backend v2 with mysql behind it

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3
 MAINTAINER Petr Novák 2 <petr.novak2@firma.seznam.cz>
 
-RUN pip install flask
+RUN pip install flask mysqlclient
 COPY backend.py /app/backend.py
 
 EXPOSE 80
--- backend.py
+++ backend.py
@@ -1,12 +1,70 @@
 import socket
 import json
-from flask import Flask
+import MySQLdb
+import os
+from contextlib import closing
+from flask import Flask, g
+
+
+class NumberStorage:
+    def _env(self, key):
+        """ Requires MYSQL_* env value """
+        value = os.getenv('MYSQL_' + key)
+        if value is None:
+            raise ValueError('Missing env MYSQL_' + key)
+        return value
+
+    def __init__(self):
+        self.db = MySQLdb.connect(
+            host=self._env('HOST'),
+            user=self._env('USER'),
+            passwd=self._env('PASSWORD'),
+            db=self._env('DATABASE'),
+            use_unicode=True)
+        self._initialize_db()
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        if self.db:
+            self.db.close()
+        self.db = None
+
+    def _initialize_db(self):
+        with closing(self.db.cursor()) as c:
+            c.execute("CREATE TABLE IF NOT EXISTS numbers (name VARCHAR(10) PRIMARY KEY, value INT UNSIGNED)")
+            c.execute("INSERT IGNORE INTO numbers (name, value) VALUES ('the_number', 0)")
+
+    def increment_and_get_number(self):
+        with closing(self.db.cursor()) as c:
+            try:
+                c.execute('START TRANSACTION')
+                c.execute("UPDATE numbers SET value = value + 1 WHERE name = 'the_number'")
+                c.execute("SELECT value FROM numbers WHERE name = 'the_number'")
+                new_value = c.fetchone()[0]
+                c.execute('COMMIT')
+                return new_value
+            except Exception:
+                c.execute('ROLLBACK')
+                raise
 
 
 app = Flask(__name__)
 
-global piggy_global_number
-piggy_global_number = 0
+
+def get_store():
+    if 'store' not in g:
+        g.store = NumberStorage()
+    return g.store
+
+
+@app.teardown_appcontext
+def teardown_store(exc):
+    store = g.pop('store', None)
+    if store is not None:
+        store.close()
+
 
 @app.route("/")
 def hello():
@@ -15,14 +73,19 @@ def hello():
 
 @app.route("/backend")
 def cat():
-    global piggy_global_number
-    piggy_global_number += 1
+    number = get_store().increment_and_get_number()
 
     return json.dumps({
         'status': 200,
-        'number': piggy_global_number,
+        'number': number,
         'hostname': socket.gethostname(),
     })
+
+@app.route("/ready")
+def ready():
+    # check we have store
+    get_store()
+    return 'OK'
 
 
 if __name__ == "__main__":
